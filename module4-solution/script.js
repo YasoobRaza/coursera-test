48aeb0eba9e47c2d42027165542ac601f2af5a62
Yasoob Raza <yasoob.raza7@gmail.com>
2021-10-16 15:28:00 +0500

    module 5 assignment

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -54,12 +54,11 @@ WARNING!!! WARNING!!!
 // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
 // name in the loop.
 
+
 (function () {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
-
     var firstLetter = names[i].charAt(0).toLowerCase();
-
     if (firstLetter === 'j') {
       ByeSpeaker.speak(names[i]);
     } else {
